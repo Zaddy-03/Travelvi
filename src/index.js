a938ecc40afe948a9f6a0a944cf1d5e7e7f88602
samod mukadam <samodmukadam2005@gmail.com>
2022-10-14 19:42:44 -0700

    add initial HTML file with

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+import Image1 from '../asset/img/image1.png';
+import Image2 from '../asset/img/image2.png';
+import Image3 from '../asset/img/image3.png';
+
+import Gallery1 from '../asset/img/gallery1.png';
+import Gallery2 from '../asset/img/gallery2.png';
+import Gallery3 from '../asset/img/gallery3.png';
+import Gallery4 from '../asset/img/gallery4.png';
+import Gallery5 from '../asset/img/gallery5.png';
+import Gallery6 from '../asset/img/gallery6.png';
+
+import Image4 from '../asset/img/image4.png';
+
+import Travelvi from './js/travelvi.js';
+
+$(".image1").attr("src", Image1);
+$(".image2").attr("src", Image2);
+$(".image3").attr("src", Image3);
+
+$(".gallery1").attr("src", Gallery1);
+$(".gallery2").attr("src", Gallery2);
+$(".gallery3").attr("src", Gallery3);
+$(".gallery4").attr("src", Gallery4);
+$(".gallery5").attr("src", Gallery5);
+$(".gallery6").attr("src", Gallery6);
+
+$(".image4").attr("src", Image4);
